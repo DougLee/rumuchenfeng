00ce0585e75f947d3fcda751989074246a29318d
李道民 <douglee1989@gmail.com>
2018-12-21 19:08:01 +0800

    add httpclient utils

--- src/main/java/com/douglee/knowledgebase/utils/HttpResponseUtils.java
+++ src/main/java/com/douglee/knowledgebase/utils/HttpResponseUtils.java
@@ -1,7 +1,35 @@
 package com.douglee.knowledgebase.utils;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+
 /**
  * Created by Douglee on 2018/12/6.
+ * HttpClient 连接池
  */
 public class HttpResponseUtils {
+
+    private static final PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
+
+    static {
+        // 项目中读取配置文件为宜
+        connManager.setMaxTotal(500);
+        connManager.setDefaultMaxPerRoute(20);
+    }
+
+    public static CloseableHttpClient getHttpClient(Integer timeout){
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setSocketTimeout(timeout)
+                .setConnectTimeout(timeout)
+                .setConnectionRequestTimeout(timeout)
+                .build();
+
+        return HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(connManager)
+                .build();
+    }
+
 }
